1dc3e5ccc2e0c6b1814167a23e3f94217e505964
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-04-26 19:12:49 +0600

    Indicator fix

--- components/for_pages/RegPage/Indicator/Item/index.tsx
+++ components/for_pages/RegPage/Indicator/Item/index.tsx
@@ -1,6 +1,8 @@
 import ColSvg from '@/components/svg/ColSvg'
 import styles from './index.module.scss'
 import classNames from 'classnames'
+import CheckSvg from '@/components/svg/CheckSvg'
+import { colors } from '@/styles/variables'
 
 interface IOption {
   text: string
@@ -17,7 +19,9 @@ export default function Item(props: Props) {
 
   return (
     <div className={styles.root}>
-      <ColSvg className={styles.col} step={props.step} index={props.index + 1} />
+      {props.step > props.index + 1 ? <CheckSvg className={styles.check} color={colors.blue500} />
+        :
+        <ColSvg className={styles.col} step={props.step} index={props.index + 1} />}
       <div className={classNames(styles.text, { [styles.active]: props.step === props.index + 1 })}>
         {props.option.text}
       </div>
