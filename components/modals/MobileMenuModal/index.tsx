72976d6f3710ddf34c725c3a9ec596842ddca65a
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-05-19 18:33:17 +0600

    Header mobile

--- components/modals/MobileMenuModal/index.tsx
+++ components/modals/MobileMenuModal/index.tsx
@@ -7,6 +7,11 @@ import BottomSheetHeader from '@/components/layout/BottomSheet/BottomSheetHeader
 import BottomSheetBody from '@/components/layout/BottomSheet/BottomSheetBody'
 import BottomSheetFooter from '@/components/layout/BottomSheet/BottomSheetFooter'
 import Link from 'next/link'
+import LoginSvg from '@/components/svg/LoginSvg'
+import UserSvg from '@/components/svg/UserSvg'
+import SettingsSvg from '@/components/svg/SettingsSvg'
+import CashSvg from '@/components/svg/CashSvg'
+import LogoutSvg from '@/components/svg/LogoutSvg'
 
 interface Props {
   isBottomSheet?: boolean
@@ -19,7 +24,22 @@ const MobileMenuModalInner = (props: Props) => {
   const menuNotAuth = [
     { link: '', label: 'Пункты приёма лома' },
     { link: '', label: 'Купить лом' },
+    { link: '/auth/login', label: 'Войти', icon: <LoginSvg color={colors.white} /> },
+    { link: '/auth/registration', label: 'Зарегистрироваться', icon: <UserSvg color={colors.white} /> }
   ]
+
+  const menuLogged = [
+    { link: '', label: 'Настройки профиля', icon: <SettingsSvg color={colors.white} /> },
+    { link: '', label: 'Оплата сервиса Jumet', icon: <CashSvg color={colors.white} /> },
+    { link: '', label: 'Выйти', icon: <LogoutSvg color={colors.white} /> },
+  ]
+
+  const handleCloseMobileMenu = () => {
+    if (typeof window !== undefined) {
+      document.body.classList.remove('modal-open')
+    }
+    appContext.hideModal()
+  }
 
   const body = (
     <>
@@ -45,11 +65,32 @@ const MobileMenuModalInner = (props: Props) => {
   else {
     return (
       <ModalLayout className={styles.modalLayout}  >
-        {menuNotAuth.map((i, index) =>
-          <Link key={index} className={styles.link} href={i.link}>
-            {i.label}
-          </Link>
-        )}
+        {!appContext.isLogged ? <>
+          {menuNotAuth.slice(0, 2).map((i, index) =>
+            <Link key={index} className={styles.link} href={i.link}>
+              {i.label}
+            </Link>
+          )}
+          <div className={styles.sep} />
+          {menuNotAuth.slice(2).map((i, index) =>
+            <Link onClick={handleCloseMobileMenu} key={index} className={styles.linkWithIcon} href={i.link}>
+              <span>{i.icon}</span>{i.label}
+            </Link>
+          )}
+        </> :
+          <>
+            {menuLogged.slice(0, 2).map((i, index) =>
+              <Link onClick={handleCloseMobileMenu} key={index} className={styles.linkWithIcon} href={i.link}>
+                <span>{i.icon}</span>{i.label}
+              </Link>
+            )}
+            <div className={styles.sep} />
+            {menuLogged.slice(2).map((i, index) =>
+              <Link onClick={() => setTimeout(() => appContext.logout(), 100)} key={index} className={styles.linkWithIcon} href={i.link}>
+                <span>{i.icon}</span>{i.label}
+              </Link>
+            )}
+          </>}
       </ModalLayout>
     )
   }
