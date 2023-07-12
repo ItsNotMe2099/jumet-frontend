f0c26378ba4ddc690cfe0beb49106987c965bbb6
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-07-12 14:16:43 +0300

    fix

--- data/temp/points.tsx
+++ data/temp/points.tsx
@@ -7,7 +7,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 31',
       isDelivery: true, haveLoading: true, opens: '9', closes: '23', rating: '4.8', id: 1, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 0,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -42,7 +42,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: false, haveLoading: false, rating: '4.8', alwaysOpen: true, id: 2, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 5,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -77,7 +77,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: false, opens: '9', closes: '13', rating: '4.8', id: 3, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 0,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -112,7 +112,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: false, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 4, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 0,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -147,7 +147,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 5, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 0,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -182,7 +182,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 6, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 5,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -217,7 +217,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 7, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 10,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -252,7 +252,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 8, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 10,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -287,7 +287,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 9, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 10,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
@@ -322,7 +322,7 @@ export const points = {
       title: 'МеталлВторЧермет', price: '23000', address: 'г. Сергиев Посад, ул. Зои Космодемьянской, 32',
       isDelivery: true, haveLoading: true, opens: '9', closes: '13', rating: '4.8', id: 10, phone: '+ 7 (495) 256-34-34',
       inn: '7714886683', entity: 'ООО «Лебедев и Партнеры»', ogrn: '1127747034040', legalAddress: '123290, город Москва, тупик Магистральный 1-й, дом 5А, оф. D504, к.8',
-      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf',
+      license: 'http://ncz.tj/system/files/Legislation/2_ru.pdf', dealsPerMonth: 10,
       zones: [{ id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }, { id: 1, distance: 'От 1 до 20 км', price: '1 000 ₽/т' }],
       employees: [
         {
