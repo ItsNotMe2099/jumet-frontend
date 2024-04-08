768a639ccffd29aa4cf8bb61e57c97234348b868
Andrey <a1081515@yandex.ru>
2024-04-08 13:43:52 +0300

    ix

--- components/for_pages/Common/Cards/DealOfferOwnerCard/index.tsx
+++ components/for_pages/Common/Cards/DealOfferOwnerCard/index.tsx
@@ -30,7 +30,7 @@ const DealOfferOwnerCardInner = ({ saleRequest, dealOffer, active }: Props) => {
   const [showOffer, setShowOffer] = useState<boolean>(false)
 
   const createdAt = Formatter.formatDateRelative(dealOffer?.createdAt ?? saleRequest.createdAt)
-  const link = dealOffer?.deal ? Routes.lkDeal(dealOffer.deal.id) : (saleRequest?.receivingPointId ? Routes.saleRequestPrivate(saleRequest.id) : saleRequest ? Routes.saleRequest(saleRequest.id) : '')
+  const link = dealOffer?.deal ? Routes.lkDeal(dealOffer.deal.id) : (saleRequest?.receivingPointId ? Routes.saleRequestPrivate(saleRequest.id) : saleRequest ? Routes.saleRequest(saleRequest.id) : '#')
   const handleShowOffer: MouseEventHandler = (e) => {
     e.preventDefault()
     e.stopPropagation()
@@ -45,7 +45,7 @@ const DealOfferOwnerCardInner = ({ saleRequest, dealOffer, active }: Props) => {
               <Link  href={link} className={styles.weight}>
                 {(saleRequest?.weight ?? 0) > 0 ? WeightUtils.formatWeight(saleRequest.weight) : 'Вес не указан'}
               </Link>
-              <Link href={(saleRequest.receivingPointId ? Routes.saleRequestPrivate(saleRequest.id) : Routes.saleRequest(saleRequest.id))} className={styles.number}>
+              <Link href={(saleRequest?.receivingPointId ? Routes.saleRequestPrivate(saleRequest.id) :  saleRequest ? Routes.saleRequest(saleRequest.id) : '#')} className={styles.number}>
                 Заявка №{saleRequest.id}
               </Link>
               {dealOffer?.deal && <Link href={Routes.lkDeal(dealOffer.deal.id)} className={styles.number}>
