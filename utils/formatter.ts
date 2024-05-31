238b577d6f0ece90c2b3e4ebabcaa52aefb401ff
Andrey <a1081515@yandex.ru>
2024-05-31 14:20:52 +0300

    fix price formatter
    (cherry picked from commit 170d8f25869f0ef76002b7af0a9c771f09e06625)

--- utils/formatter.ts
+++ utils/formatter.ts
@@ -91,7 +91,7 @@ export default class Formatter {
     if (!price) {
       return ''
     }
-    return `${this.formatNumber(Math.ceil(price))} ${suffix ?? '₽'}`
+    return `${this.formatNumber(price)} ${suffix ?? '₽'}`
   }
   static formatDeliveryPrice(price?: number): string {
     if (!price) {
